1d0730b3f9ee2a91861b70aeceb01b8ca8545948
Sufan Chou <sufanc@gmail.com>
2020-08-10 23:40:25 -0700

    Initial Commit

---
+++ Shared/PotpourriApp.swift
@@ -0,0 +1,17 @@
+//
+//  PotpourriApp.swift
+//  Shared
+//
+//  Created by sufan on 8/10/20.
+//
+
+import SwiftUI
+
+@main
+struct PotpourriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
